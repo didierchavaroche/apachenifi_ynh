eb1fee9cf279cfc4240b7aa3833a0bda2fc24152
Didier Chavaroche <didier@chavaroche-si.fr>
2025-12-30 14:38:11 +0100

    setting up javahome directory

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,3 +4,13 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
+#=================================================
+#
+#=================================================
+
+_install_nifi_env() {
+    echo "JAVA_HOME="$(find / -name jdk-21*) > $install_dir/.env
+    echo "NIFI_HOME="$install_dir >> $install_dir/.env
+
+    chown $app:$app $install_dir/.env
+}
