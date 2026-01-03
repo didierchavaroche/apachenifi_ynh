f5cc190473e16c0fb3638dd45db2b06801784986
Didier Chavaroche <didier@chavaroche-si.fr>
2026-01-04 00:23:26 +0100

    correction d'une faute de frappe

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -20,12 +20,13 @@ __install_keystore() {
     openssl pkcs12 -export -out $install_dir/nifi.p12 -inkey /etc/yunohost/certs/$domain/key.pem -in /etc/yunohost/certs/$domain/crt.pem -name nifi-key -passout pass:$keystorepasswd
 
     # Convert the PKCS12 keystore for the NiFi instance into the Java KeyStore file (keystore.jks).
-    keytool -storepasswd -new $keystorepasswd -importkeystore -srckeystore $install/nifi.p12 -srcstoretype pkcs12 -srcalias nifi-key -destkeystore $install_dir/conf/keystore.jks -deststoretype jks -destalias nifi-key
+    keytool -storepasswd -new $keystorepasswd -importkeystore -srckeystore $install_dir/nifi.p12 -srcstoretype pkcs12 -srcalias nifi-key -destkeystore $install_dir/conf/keystore.jks -deststoretype jks -destalias nifi-key
 
     # Convert the CA certificate into the NiFi truststore (truststore.jks) to allow trusted incoming connections.
     keytool -storepasswd -new $keystorepasswd -importcert -alias nifi-cert -file $install_dir/nifi-cacert.pem -keystore $install_dir/conf/truststore.jks
 
     # remove temporary certificate
+    ynh_safe_rm $install_dir/nifi.p12
     ynh_safe_rm $install_dir/nifi-cacert.pem
 
     #add reading permissions to user
