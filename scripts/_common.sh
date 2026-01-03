854953f95739e94cd5d3cea0b91288925e5291b3
Didier Chavaroche <didier@chavaroche-si.fr>
2026-01-03 11:42:47 +0100

    ajout du keystore

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,3 +5,12 @@
 #=================================================
 
 java_home=$install_dir/jvm
+keystorepasswd=$(ynh_string_random --length=12)
+
+run__install_keystore() {
+    if [ -f "$install_dir/conf/keystore.jks" ]; then
+        rm $install_dir/conf/keystore.jks
+    fi
+
+    keytool -genkeypair -alias nifi -keyalg RSA -keystore $install_dir/conf/keystore.jks -storepass $keystorepasswd -keypass $keystorepasswd -validity 365 -keysize 2048 -dname "CN=$domain,OU=users"
+}
